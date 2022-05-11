416e82cd8d5d1248db28d209cab1a4ec07a28e20
JordanPauls <66690811+JordanPauls@users.noreply.github.com>
2022-05-11 08:24:58 -0400

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,5 +1,17 @@
-print("Hello World")
+import datetime
+import subprocess
+import time
 
-
-with open('/home/pi/output/SimpleFile2.txt', 'w') as f:
-    f.write('SimpleLine14')
+while True:
+    try:
+        ping = subprocess.call('ping -c 1 www.google.com')
+    except:
+        ping = "Error on raspberry pi side"
+    print(str(datetime.datetime.now())+'\t'+str(ping))
+    
+    with open('/home/pi/Desktop/Connectivity.txt', 'a') as f:
+        f.write(str(datetime.datetime.now()))
+        f.write('\t')
+        f.write(str(ping))
+        f.write('\n')
+    time.sleep(60)
